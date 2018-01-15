6dbae09887509a2b566bdc70a7aca3c26505a36c
Matt Van Der Westhuizen <mattpwest@gmail.com>
2018-01-15 14:14:10 +0200

    Added solution for exercise 1.

---
+++ src/main/java/za/co/entelect/bootcamp/controllers/PublisherController.java
@@ -0,0 +1,15 @@
+package za.co.entelect.bootcamp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PublisherController {
+
+    @GetMapping("")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+}
